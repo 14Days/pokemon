be7c12ec484fa0649b01c1595d80cf5e554ddd8e
wangjq4214 <w1637894214@163.com>
2019-12-17 23:17:40 +0800

    调整transform

--- data/transforms.py
+++ data/transforms.py
@@ -27,18 +27,18 @@ _image_transforms = {
         ]),
     'BnOpt_train':
         transforms.Compose([
-            transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+            transforms.RandomResizedCrop(size=200, scale=(0.8, 1.0)),
             transforms.RandomRotation(degrees=15),
             transforms.ColorJitter(),
             transforms.RandomHorizontalFlip(),
-            transforms.CenterCrop(size=200),
+            # transforms.CenterCrop(size=200),
             transforms.ToTensor(),
             _normal_transform
         ]),
     'BnOpt_test':
         transforms.Compose([
-            transforms.Resize(size=256),
-            transforms.CenterCrop(size=200),
+            transforms.Resize(size=200),
+            # transforms.CenterCrop(size=200),
             transforms.ToTensor(),
             _normal_transform
         ])
